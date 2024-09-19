19468c8e590565850c1bd71bce24804619bddff1
SageLovedayy <116376154+SageLovedayy@users.noreply.github.com>
2024-09-19 04:58:39 +0100

    Implement MVC model

--- server.js
+++ server.js
@@ -1,7 +1,26 @@
+const mongoose = require('mongoose');
 const app = require('./app');
+const dotenv = require('dotenv');
+dotenv.config({ path: './config.env' });
 
-const port = 3000;
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD,
+);
 
+mongoose
+  //  .connect(process.env.DATABASE_LOCAL, { //FOR LOCAL DATABASE
+  .connect(DB, {
+    // FOR REMOTE DATABASE
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log('DB connection successful!'));
+
+
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}`);
 });
